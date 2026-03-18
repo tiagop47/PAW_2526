d7896bb210eded4f25f8a28101674639b1f59045
Jose Tiago <8240128@estg.ipp.pt>
2026-03-18 15:42:27 +0000

    0.0.0

--- routes/auth.js
+++ routes/auth.js
@@ -1,25 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-  /**
-   * Rota para login de utilizadores.
-   */
-router.get('/login', function(req, res, next) {
-  res.render('loginRegistro/login', { errorMessage: null }); 
+/**
+ * Rota para login de utilizadores.
+ */
+router.get('/login', function (req, res, next) {
+    res.render('loginRegisto/login', { errorMessage: null });
 });
 
-  /**
-   * Rota para registo de utilizadores.
-   */
-  router.get("/register", (req, res) => {
-    res.render("loginRegistro/register");
-  });
-  
-    /**
-   * Rota para registo de utilizadores.
-   */
-  router.get("/recoverPassword", (req, res) => {
-    res.render("loginRegistro/recoverPassword");
-  });
+/**
+ * Rota para registo de utilizadores.
+ */
+router.get("/register", (req, res) => {
+    res.render("loginRegisto/register");
+});
+
+/**
+* Rota para registo de utilizadores.
+*/
+router.get("/recoverPassword", (req, res) => {
+    res.render("loginRegisto/recoverPassword");
+});
 
 module.exports = router;
