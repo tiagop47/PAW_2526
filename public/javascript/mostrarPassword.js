7dc404e4446e94df88d0f793dfb34a476917430b
Jose Tiago <8240128@estg.ipp.pt>
2026-03-18 17:15:39 +0000

    0.0.0

--- public/javascript/mostrarPassword.js
+++ public/javascript/mostrarPassword.js
@@ -1,14 +1,23 @@
-const pass = document.getElementById("password");
-const btnToggle = document.getElementById("togglePassword");
+const palavraPasse = document.getElementById("password");
+const botaoAlternar = document.getElementById("togglePassword");
 
-if (pass && btnToggle) {
-    btnToggle.addEventListener("click", () => {
-        if (btnToggle.innerText === "Ver") {
-            pass.type = "text";
-            btnToggle.innerText = "Ocultar";
-        } else {
-            pass.type = "password";
-            btnToggle.innerText = "Ver";
+let valorOriginal = palavraPasse.value;
+let estaMascarada = false;
+
+botaoAlternar.addEventListener("click", () => {
+    if (estaMascarada) {
+        palavraPasse.value = valorOriginal;
+        botaoAlternar.innerText = "Ocultar";
+        estaMascarada = false;
+    } else {
+        valorOriginal = palavraPasse.value;
+        let valorMascarado = "";
+        for (let indice = 0; indice < valorOriginal.length; indice++) {
+            valorMascarado += "*";
         }
-    });
-}
+        palavraPasse.value = valorMascarado;
+        botaoAlternar.innerText = "Ver";
+        estaMascarada = true;
+    }
+});
+
