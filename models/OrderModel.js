b7a0d87d3128dd21b2d7a597325038228783e1b1
Jose Tiago <8240128@estg.ipp.pt>
2026-03-28 18:44:06 +0000

    1.08
    pequena correçao enum

--- models/OrderModel.js
+++ models/OrderModel.js
@@ -15,7 +15,7 @@ const OrderSchema = new mongoose.Schema({
     valorTotal: { type: Number, required: true },
     estado: {
         type: String,
-        enum: ['pendente', 'confirmada', 'em entrega', 'cancelada'],
+        enum: ['pendente', 'confirmada', 'em entrega', 'entregue', 'cancelada'],
         default: 'pendente'
     },
     metodoEntrega: {
