05fa7abee83ecfb703e578105b780f0fbde91dd6
Gonçalo Maia <masteryt207@gmail.com>
2026-04-27 23:23:26 +0100

    v2.0.4
    Alteraçoes navbar

--- frontoffice/src/app/app.component.ts
+++ frontoffice/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NotificationComponent } from './components/notification/notification.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NotificationComponent],
+  imports: [RouterOutlet, NotificationComponent, NavbarComponent],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
